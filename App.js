9fd14e3e7509665e4a321fd4b2a7a4ff999dc166
6derick6 <6derick6@gmail.com>
2023-05-24 13:15:35 -0300

    App noticias 1/4

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, Button, ScrollView, ImageBackground, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import * as firebase from 'firebase';
 
-export default function App() {
+function HomeScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1}}>
+
+     <ScrollView horizontal contentContainerStyle={{height:300, width:'200%'}} style={{flex:1}}>
+      <ImageBackground source={{uri: 'https://picsum.photos/200/300'}} style={styles.image}>
+        <View style={{position:'absolute',left:0,top:0,width:'100%',height:'100%',backgroundColor:'rgba(0,0,0,0.5)'}}></View>
+        <Text style={{fontSize:29, color:'white'}}>A minha Notícia!</Text>
+      </ImageBackground>
+     </ScrollView>
+
     </View>
   );
 }
 
+function NoticiaScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Portal" component={HomeScreen} />
+        <Tab.Screen name="Noticia" component={NoticiaScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  image: {
+    flex:1,
+    resizeMode:'cover',
+    justifyContent:'flex-end',
+    width:'50%'
+  }
 });
